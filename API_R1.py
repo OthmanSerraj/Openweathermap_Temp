8c539f3c70b3088de07200be497291d376ed91e4
OthmanSerraj <51855596+OthmanSerraj@users.noreply.github.com>
2021-03-26 21:40:36 +0100

    Add query form
    useful in the case of several params

--- API_R1.py
+++ API_R1.py
@@ -1,15 +1,13 @@
 from LWA import LWA
 from fastapi import FastAPI
 R1_app = FastAPI()
-@R1_app.get('/api/meteo/vent/{ville}')
-async def index(ville):
+@R1_app.get('/api/meteo/vent/')
+async def get_speed_deg(ville:str):
     New_ville_infos=LWA(ville)
-    New_ville_infos.get_speed()
-    New_ville_infos.get_deg()
     return {"vitesse":New_ville_infos.get_speed(),
     "degre":New_ville_infos.get_deg()}
-@R1_app.get('/api/meteo/tempmax/{nville}')
-async def index(nville):
-    ville_infos=LWA(nville)
+@R1_app.get('/api/meteo/tempmax/')
+async def get_maxtemp_5day(ville:str):
+    ville_infos=LWA(ville)
     return ville_infos.get_max_tempnhday()
-#Execute: uvicorn API_R1:R1_app --reload
+#Execute: uvicorn API_R1:R1_app --reload, example: http://localhost:8000/api/meteo/vent/?ville=paris or http://localhost:8000/api/meteo/tempmax/?ville=dakar
