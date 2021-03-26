d2eeee5d6c4342c39ef89b53f70f15f55ce2be4b
OthmanSerraj <51855596+OthmanSerraj@users.noreply.github.com>
2021-03-26 21:34:45 +0100

    Compact version

--- LWA.py
+++ LWA.py
@@ -23,40 +23,33 @@ class LWA:
         r = requests.get(url)
         return r.json()
     @staticmethod
-    def day_num(mydict,i=0):
-        x=mydict["list"][i]["dt_txt"].split()[0]
-        for num,elm in enumerate(mydict["list"][i:]):
-            if elm["dt_txt"].split()[0]!=x:return num
-        return 0
-    def get_max_tempnhday(self):
-        begin_num=self.day_num(self.city_5days_infos)
-        zero_day=self.city_5days_infos["list"][begin_num]["dt_txt"].split()[0]
-        maxtp=-273.15#zero absolu
-        wanted_date=""
-        Lista=[]
-        xp=0
-        for num,elm in enumerate(self.city_5days_infos["list"][begin_num:]):
-            date=elm["dt_txt"]
-            actual_day=date.split()[0]
-            if zero_day!=actual_day:
-                xp=num
-                Lista.append({"Date":wanted_date,"max_temp":maxtp})
-                zero_day=actual_day
-                maxtp=-273.15#zero absolu
-            mytemp=elm["main"]["temp_max"]
-            if mytemp>maxtp:
-                maxtp=mytemp
-                wanted_date=elm["dt_txt"]
-        end_num=xp+1
+    def day_num(mydict):
+        L_day=[]
+        x=mydict["list"][0]["dt_txt"].split()[0]
+        for num,elm in enumerate(mydict["list"]):
+            selm=elm["dt_txt"].split()[0]
+            if x!=selm:
+                x=selm
+                L_day.append(num)
+        return L_day+[mydict["cnt"]]
+    @staticmethod
+    def get_maxtpday(Stock):
+        '''calcul le max de temp de un dict ici par jour'''
         maxtp=-273.15
-        for elm in self.city_5days_infos["list"][end_num:]:
+        for elm in Stock:
             mytemp=elm["main"]["temp_max"]
             if mytemp>maxtp:
                 maxtp=mytemp
                 wanted_date=elm["dt_txt"]
-        Lista.append({"Date":wanted_date,"max_temp":maxtp})
+        return {"Date":wanted_date,"max_temp":maxtp}
+    def get_max_tempnhday(self):
+        V=self.day_num(self.city_5days_infos)
+        S={}
+        Lista=[]
+        for a,b in zip(V,V[1:]) :
+            S=self.get_maxtpday(self.city_5days_infos["list"][a:b])
+            Lista.append(S)
         return Lista
-
     def get_speed(self):
         speed=self.city_last_infos["wind"]["speed"]
         return speed
