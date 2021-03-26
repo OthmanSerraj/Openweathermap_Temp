65f005633acc371205bd4a4494a2565375b12ecb
OthmanSerraj <51855596+OthmanSerraj@users.noreply.github.com>
2021-03-26 17:09:34 +0100

    add get list of 5 max temp by day

--- LWA.py
+++ LWA.py
@@ -23,15 +23,22 @@ class LWA:
         r = requests.get(url)
         return r.json()
     def get_max_tempnhday(self):
+        zero_day=self.city_5days_infos["list"][0]["dt_txt"].split()[0]
         maxtp=-273.15#zero absolu
-        znum=0
+        wanted_date=""
+        Lista=[]
         for num,elm in enumerate(self.city_5days_infos["list"]):
+            date=elm["dt_txt"]
+            actual_day=date.split()[0]
+            if zero_day!=actual_day:
+                Lista.append({"Date":wanted_date,"max_temp":maxtp})
+                zero_day=actual_day
+                maxtp=-273.15#zero absolu
             mytemp=elm["main"]["temp_max"]
             if mytemp>maxtp:
                 maxtp=mytemp
-                znum=num
-        date=self.city_5days_infos["list"][znum]["dt_txt"]
-        return {"Date":date,"max_temp":maxtp}
+                wanted_date=elm["dt_txt"]
+        return Lista
 
     def get_speed(self):
         speed=self.city_last_infos["wind"]["speed"]
@@ -44,7 +51,7 @@ class LWA:
         with open(file_store_temp,"w") as fr:
             json.dump(self.city_5days_infos, fr,indent=3)
         if open_show:os.startfile(file_store_temp)
-if __name__== "__main__":
-    location="dublin"
-    Z=LWA(location)
-    print(Z.get_last_weather())
+# if __name__== "__main__":
+#     location="paris"
+#     Z=LWA(location)
+#     print(Z.get_last_weather())
